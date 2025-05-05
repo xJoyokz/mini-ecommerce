bc07e72ec0567e5c06497fd8cc7d588eb37a9f9b
Jaya Wicaksana <jaya@itsavirus.com>
2025-05-05 19:41:54 +0800

    fix: fix the details page for deployment

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,4 +1,11 @@
+import React from 'react'
 import DetailsItem from './DetailsItem'
+
+export async function generateStaticParams() {
+  // List all the IDs you want to pre-render
+  // For fix the error of the product not found
+  return [{ id: '1' }, { id: '2' }, { id: '3' }]
+}
 
 export default function ProductPage() {
   return <DetailsItem />
